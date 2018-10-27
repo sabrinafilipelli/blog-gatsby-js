dfc510bbca3a5b8aaee9e91e7b3c0ddcb80f74bf
SabrinaGear <sabrinagear.ft@gmail.com>
2018-10-27 01:35:32 -0500

    commit

---
+++ content/meta/config.js
@@ -0,0 +1,32 @@
+const colors = require("../../src/styles/colors");
+
+module.exports = {
+  siteTitle: "Sabrina Gear Blog", // <title>
+  shortSiteTitle: "My Personal Blog", // <title> ending for posts and pages
+  siteDescription: "Sabrina Gear personal blog site",
+  siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
+  pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+  // author
+  authorName: "sabrina gear",
+  authorTwitterAccount: "sabrinagear",
+  // info
+  infoTitle: "sabrina gear",
+  infoTitleNote: "personal blog",
+  // manifest.json
+  manifestName: "PersonalBlog - a personal blog",
+  manifestShortName: "PersonalBlog", // max 12 characters
+  manifestStartUrl: "/",
+  manifestBackgroundColor: colors.background,
+  manifestThemeColor: colors.background,
+  manifestDisplay: "standalone",
+  // contact
+  contactEmail: "sabrinageardesigns@gmail.com.com",
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/sabrinagear" },
+    { name: "twitter", url: "https://twitter.com/sabrina_gear" },
+    { name: "facebook", url: "https://www.facebook.com/mrs.s.gear" }
+  ]
+};
---
+++ content/pages/1--about/index.md
@@ -0,0 +1,7 @@
+---
+title: About
+---
+
+![Self](./sabrina_gear.jpg)
+
+My name is Sabrina Gear. I am a web developer, designer, and photographer based in the greater Nashville area.
