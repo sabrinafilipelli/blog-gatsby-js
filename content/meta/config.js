104d868c9a948672f6d35d5f70c3ef1ff309be43
Sabrina Filipelli <41698855+sabrinafilipelli@users.noreply.github.com>
2022-06-27 20:45:11 -0500

    Update config.js

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,16 +1,16 @@
 const colors = require("../../src/styles/colors");
 
 module.exports = {
-  siteTitle: "Sabrina Gear Blog", // <title>
+  siteTitle: "Sabrina Filipelli Blog", // <title>
   shortSiteTitle: "My Personal Blog", // <title> ending for posts and pages
-  siteDescription: "Sabrina Gear personal blog site",
+  siteDescription: "Sabrina Filipelli Gatsby blog site",
   siteUrl: "https://unruffled-torvalds-fcf3e6.netlify.com",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
-  authorName: "sabrina gear",
-  authorTwitterAccount: "sabrinagear",
+  authorName: "sabrina filipelli",
+  authorTwitterAccount: "sabrinafilipelli",
   // info
   infoTitle: "sabrina gear",
   infoTitleNote: "personal blog",
@@ -25,8 +25,8 @@ module.exports = {
   contactEmail: "sabrinageardesigns@gmail.com.com",
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/sabrinagear" },
-    { name: "twitter", url: "https://twitter.com/sabrina_gear" },
-    { name: "facebook", url: "https://www.facebook.com/mrs.s.gear" }
+    { name: "github", url: "https://github.com/sabrinafilipelli" },
+    { name: "twitter", url: "https://twitter.com/sabrinafilipelli" },
+    { name: "facebook", url: "https://www.facebook.com/" }
   ]
 };
